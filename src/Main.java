766448cdd685fa49d6f8f34a5a8024aefa5d3e84
ljb15103508897 <liu15103508897>
2018-11-23 15:26:09 +0800

    这是什么操作

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("哈哈哈");
+        System.out.println("哈哈哈");
+        System.out.println("哈哈哈");
+        System.out.println("这个是什么鬼东西");
     }
 
 
