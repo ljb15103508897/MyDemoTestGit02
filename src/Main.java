fabc84ddfa970222f1232d5fb47a697bcfb371a8
ljb15103508897 <liu15103508897>
2018-11-23 15:24:15 +0800

    好多次提交

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import org.junit.Test;
 
 public class Main {
 
-    @Test
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+
+
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
     }
